4797e3fd486185eb3ee304c3f020bd90788fdad9
lynn1286 <lynn1286@126.com>
2025-06-11 17:53:43 +0800

    feat: :heavy_plus_sign: 校验 git message 规范

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,7 +1,7 @@
-const lintStagedConfig = {
+const Configuration = {
   '**/*.(ts|tsx)': () => [`pnpm exec tsc --noEmit`],
   '**/*.(ts|tsx|js)': () => [`pnpm run format:check`, `pnpm run lint --quiet`],
   '**/*.(md|json|css|scss)': () => [`pnpm run format:check`],
 };
 
-export default lintStagedConfig;
+export default Configuration;
