f5ed7b97ae70e5c98e6a659900964e79b9385a5a
lynn1286 <lynn1286@126.com>
2025-06-11 16:42:23 +0800

    feat: :technologist: 配置 prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,10 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // 基础配置
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  // Prettier 配置放在最后，覆盖之前的规则以解决冲突
+  ...compat.extends('prettier'),
 ];
 
 export default eslintConfig;
